78e5eb722a39cefa5555082bd29d275f29d6a1a7
Houssam <houssam.naous1@gmail.com>
2021-02-16 19:19:29 +0100

    added ability to detect plane addition and updates in a scene

--- LavaFloor/ViewController.swift
+++ LavaFloor/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import ARKit
+class ViewController: UIViewController, ARSCNViewDelegate {
 
-class ViewController: UIViewController {
-
+    let configuration = ARWorldTrackingConfiguration()
+    @IBOutlet weak var sceneView: ARSCNView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.sceneView.debugOptions = [SCNDebugOptions.showWorldOrigin, SCNDebugOptions.showFeaturePoints]
+        self.configuration.planeDetection = .horizontal
+        self.sceneView.session.run(configuration)
+        self.sceneView.delegate = self // delegate overrides are defined in the class itself
     }
 
-
+    // Called when a horizontal plane is detected
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        print("new flat surface detected, new ARPlaneAnchor added")
+    }
+    
+    // Called when the plane is updated.
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        print("updating floor's anchor ... ")
+    }
+    
+    // Called when a plane is removed ( when by mistake two planes are created for the same surface ? )
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        print("removed an anchor ..")
+    }
 }
 
