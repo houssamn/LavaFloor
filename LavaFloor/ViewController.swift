5eaada354b148a4acd8a1edf432165f86ee8bb6b
Houssam <houssam.naous1@gmail.com>
2021-02-20 22:47:43 +0100

    added Lava effect to floor planes

--- LavaFloor/ViewController.swift
+++ LavaFloor/ViewController.swift
@@ -18,24 +18,50 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         self.configuration.planeDetection = .horizontal
         self.sceneView.session.run(configuration)
         self.sceneView.delegate = self // delegate overrides are defined in the class itself
+        
     }
 
+    func createLava(planeAnchor: ARPlaneAnchor) -> SCNNode {
+        let lavaNode = SCNNode(geometry: SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z)))
+        lavaNode.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "Lava")
+        lavaNode.geometry?.firstMaterial?.isDoubleSided = true
+        lavaNode.position = SCNVector3(planeAnchor.center.x,planeAnchor.extent.y,planeAnchor.extent.z)
+        lavaNode.eulerAngles = SCNVector3(90.degreesToRadians, 0, 0)
+        return lavaNode
+    }
+    
     // Called when a horizontal plane is detected
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        
+        let lavaNode = createLava(planeAnchor: planeAnchor)
+        node.addChildNode(lavaNode)
         print("new flat surface detected, new ARPlaneAnchor added")
+        
+        
     }
     
     // Called when the plane is updated.
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
         guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
         print("updating floor's anchor ... ")
+        node.enumerateChildNodes { (childNode, _) in
+            childNode.removeFromParentNode()
+        }
+        let lavaNode = createLava(planeAnchor: planeAnchor)
+        node.addChildNode(lavaNode)
     }
     
     // Called when a plane is removed ( when by mistake two planes are created for the same surface ? )
     func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
-        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        guard let _ = anchor as? ARPlaneAnchor else {return}
         print("removed an anchor ..")
+        node.enumerateChildNodes { (childNode, _) in
+            childNode.removeFromParentNode()
+        }
     }
 }
 
+extension Int {
+    var degreesToRadians : Double { return Double(self) * .pi/180 }
+}
